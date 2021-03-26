e31018b9c32a57fd0ac7abe8ec4c5e11b82aed6e
Khalil Jad <j.khalil588@gmail.com>
2021-03-26 01:31:14 -0600

    completed task 1

--- index.js
+++ index.js
@@ -42,7 +42,24 @@ class Airplane {
   */
   
  class Person {
-    
+   constructor(name, age) {
+     this.name = name;
+     this.age = age;
+     this.stomach = [];
+   }
+    eat(edible) {
+      if(this.stomach.length < 10) {
+        this.stomach.push(edible);
+      } 
+    }
+
+    poop() {
+      this.stomach = [];
+    }
+
+    toString() {
+      return `${this.name}, ${this.age}`;
+    }
   }
   
   /*
@@ -76,7 +93,9 @@ class Airplane {
           + {name} and {location} of course come from the instance's own properties.
   */
  class Lambdasian {
-    
+    constructor(person) {
+
+    }
   }
   
   /*
