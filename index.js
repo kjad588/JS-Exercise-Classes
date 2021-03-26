d1c4b283fe8291f9f4779d942d328b11757a193e
Khalil Jad <j.khalil588@gmail.com>
2021-03-26 02:43:36 -0600

    updated tasks 5 and 6 to pass tests

--- index.js
+++ index.js
@@ -92,9 +92,9 @@ class Airplane {
     drive(distance) {
       this.odometer += distance;
       this.tank -= (distance/this.milesPerGallon);
-      if(this.tank === 0) {
-        return `I ran out of fuel at ${this.odometer}`;
-      }
+      if(this.tank < 1) {
+        return `I ran out of fuel at ${this.odometer} miles!`;
+      };
     }
   }
   
@@ -165,7 +165,28 @@ class Airplane {
           + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
           + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
   */
- class Student {
+ class Student extends Lambdasian {
+   constructor(person) {
+     super(person);
+     this.previousBackground = person.previousBackground;
+     this.className = person.className;
+     this.favSubjects = person.favSubjects;
+
+   }
+
+   listSubjects() {
+     return this.favSubjects.reduce((accumulator, item) => {
+        return accumulator + ' ' + item;
+     }, '');
+   }
+
+   PRAssignment(subject) {
+    return `${this.name} has submitted a PR for ${subject}`;
+   }
+
+   sprintChallenge(subject) {
+     return `${this.name} has begun sprint challenge on ${subject}`;
+   }
      
  }
   
@@ -182,8 +203,25 @@ class Airplane {
           + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
   */
- class ProjectManager {
-     
+ class ProjectManager extends Instructor{
+     constructor(person) {
+       super(person);
+       this.gradClassName = person.gradClassName;
+       this.favInstructor = person.favInstructor;
+
+     }
+
+     standUp(channel) {
+       return  `${this.name} announces to ${channel}, @channel standy times!`;
+     }
+
+     debugsCode(student, subject) {
+      return `${this.name} debugs ${student.name}\'s code on ${subject}`;
+     }
+
+     grade(obj, subject) {
+       return  `${subject}`
+     }
  }
   /*
     STRETCH PROBLEM (no tests!)
